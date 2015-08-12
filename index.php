c5e914efd3978902ddb02b718bae09ddb22eb575
gitawsme <gitawsme@gmail.com>
2015-08-12 15:39:48 +0300

    add vk api. show main person

--- index.php
+++ index.php
@@ -12,6 +12,74 @@
 </head>
 
 <body>
+
+    <?php
+
+    function url_get_contents($Url) {
+        if (!function_exists('curl_init')){ 
+            die('CURL is not installed!');
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $Url);
+        curl_setopt($ch, CURLOPT_PROXY, '192.168.5.111:3128');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+
+    $client_id = '5028334'; // ID приложения
+    $client_secret = '3qQyJmeL1YTph708Yp1q'; // Защищённый ключ
+    $redirect_uri = 'http://vkapi'; // Адрес сайта
+
+    $url = 'http://oauth.vk.com/authorize?';
+
+    $params = array(
+        'client_id'     => $client_id,
+        'redirect_uri'  => $redirect_uri,
+        'response_type' => 'code'
+    );
+
+    echo $link = '<p><a href="' . $url . urldecode(http_build_query($params)) . '">Аутентификация через ВКонтакте</a></p>';
+
+	if (isset($_GET['code'])) {
+    $result = false;
+    $params = array(
+        'client_id' => $client_id,
+        'client_secret' => $client_secret,
+        'code' => $_GET['code'],
+        'redirect_uri' => $redirect_uri
+    );
+
+    $token = json_decode(url_get_contents('https://oauth.vk.com/access_token' . '?' . urldecode(http_build_query($params))), true);
+
+    if (isset($token['access_token'])) {
+        $params = array(
+            'uids'         => $token['user_id'],
+            'fields'       => 'uid,first_name,last_name,screen_name,sex,bdate,photo_100',
+            'access_token' => $token['access_token']
+        );
+
+        $userInfo = json_decode(url_get_contents('https://api.vk.com/method/users.get' . '?' . urldecode(http_build_query($params))), true);
+        if (isset($userInfo['response'][0]['uid'])) {
+            $userInfo = $userInfo['response'][0];
+            $result = true;
+        }
+    }
+
+    if ($result) {
+        echo "Социальный ID пользователя: " . $userInfo['uid'] . '<br />';
+        echo "Имя пользователя: " . $userInfo['first_name'] . '<br />';
+        echo "Ссылка на профиль пользователя: " . $userInfo['screen_name'] . '<br />';
+        
+        echo "Пол пользователя: " . $userInfo['sex'] . '<br />';
+        echo "День Рождения: " . $userInfo['bdate'] . '<br />';
+        echo '<img src="' . $userInfo['photo_100'] . '" />'; echo "<br />";
+    }
+}
+?>
 	<div class="wrapper">
 		<div class="container">
 			<div class="content-center">
@@ -49,42 +117,12 @@
 							<option>HTML шаблоны</option>
 						</select>
 					</div>
-
-					<a href="https://oauth.vk.com/authorize?client_id=5028334&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,video,offline &response_type=token&v=5.36">sss</a>
-					<a href="https://api.vk.com/method/friends.get?user_id=95366042&v=5.36&access_token=d38528409ed240818f61d8115eceb85a66d20b12a759368a3ab2e439851b62520d29134cc8955b2a9b748">sss</a>
-<!-- 					<div class="main-content--vkapi">
-						<div class="send-friends">
-							<p>
-								<input type="checkbox" checked>Отправить всем друзьям
-							</p>
-
-							<ul>
-								<li><img src="img/photo.png" alt="photo"></li>
-								<li><img src="img/photo-1.png" alt="photo-1"></li>
-								<li><img src="img/photo-2.png" alt="photo-2"></li>
-								<li><img src="img/photo.png" alt="photo"></li>
-							</ul>
-							<ul>
-								<li><img src="img/photo.png" alt="photo"></li>
-								<li><img src="img/photo-1.png" alt="photo-1"></li>
-								<li><img src="img/photo-2.png" alt="photo-2"></li>
-								<li><img src="img/photo.png" alt="photo"></li>
-							</ul>
-							<ul>
-								<li><img src="img/photo.png" alt="photo"></li>
-								<li><img src="img/photo-1.png" alt="photo-1"></li>
-								<li><img src="img/photo-2.png" alt="photo-2"></li>
-								<li><img src="img/photo.png" alt="photo"></li>
-							</ul>
-							<ul>
-								<li><img src="img/photo.png" alt="photo"></li>
-								<li><img src="img/photo-1.png" alt="photo-1"></li>
-								<li><img src="img/photo-2.png" alt="photo-2"></li>
-								<li><img src="img/photo.png" alt="photo"></li>
-							</ul>
-						</div>
-					</div>
-					 -->
+					<!-- https://oauth.vk.com/blank.html -->
+					<!-- e668b7f279294fe2e1 -->
+					<a href="https://oauth.vk.com/authorize?client_id=5028334&scope=offline,friends,wall&redirect_uri=https://oauth.vk.com/blank.html &display=page&v=3.0&response_type=token">Autorize</a><br>
+					<a href="https://api.vk.com/method/wall.post?owner_id=95366042&message=ТУТ_ТЕКСТ&v=5.0&access_token=d38528409ed240818f61d8115eceb85a66d20b12a759368a3ab2e439851b62520d29134cc8955b2a9b748">Post</a><br>
+					<a href="https://api.vk.com/method/account.getAppPermissions?access_token=d38528409ed240818f61d8115eceb85a66d20b12a759368a3ab2e439851b62520d29134cc8955b2a9b748">AAAA</a><br>
+					<a href="https://api.vk.com/method/friends.get?user_id=95366042&v=5.36&access_token=d38528409ed240818f61d8115eceb85a66d20b12a759368a3ab2e439851b62520d29134cc8955b2a9b748">friends.get</a>
 				</div>
 			</div>
 		</div>
